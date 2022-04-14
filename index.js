c5b9a17f6a14508f0a40bb4344fc7c3465783612
Sean Sarokas <sps4462@gmail.com>
2022-04-14 06:08:58 -0400

    commit tree 219405359d04ba8a43452b7a6d2f287c86c4a0e7

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+const employee = {};
+employee.name = 'Sam';
+employee.streetAddress = '123 Main St';
+
+/*function beforeEach(employee, key){
+    return delete employee[key];
+}*/
+
+/*function assignEmployee(key, value){
+    const employee = {};
+    return employee[key] = value;
+}*/
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    //const updatedEmployee = {...employee};
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
